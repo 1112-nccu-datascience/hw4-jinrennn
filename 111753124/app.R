364fdead361c26532aade7c921b68d5cc6ee962e
jinrennn <51142953+jinrennn@users.noreply.github.com>
2023-05-11 15:27:56 +0800

    Update app.R

--- 111753124/app.R
+++ 111753124/app.R
@@ -1,10 +1,8 @@
 library(shiny)
 library(ggbiplot)
-
 library(DT)
 library(FactoMineR)
-library(corrplot)
-library(factoextra)
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -41,8 +39,8 @@ ui <- fluidPage(
                                  sidebarLayout(
                                    sidebarPanel(
                                      sliderInput('rangePCA', "Choose how many input data to do PCA: ", min=1, max=150, value=c(1,150)),
-                                     selectInput('pca_x', 'x axis',choices = c('PC1'=1, 'PC2'=2, 'PC3'=3, 'PC4'=4), selected = 1),
-                                     selectInput('pca_y', 'y axis',choices = c('PC1'=1, 'PC2'=2, 'PC3'=3, 'PC4'=4), selected = 2) 
+                                     selectInput('pca_x', 'x axis',choices = c('PC1'=1, 'PC2'=2, 'PC3'=3, 'PC4'=4)),
+                                     uiOutput('pca_y')
                                      ),
                                     
                                    mainPanel(plotOutput("pcaPlot"))
@@ -76,14 +74,15 @@ server <- function(input, output,session) {
   ir.pca <- reactive(prcomp(log.ir(),center = TRUE, scale. = TRUE))
   ir.ca  <- reactive(CA(iris[input$rangeCA[1]:input$rangeCA[2], 1:4], graph = FALSE))
   
-  observe({
+  output$pca_y <- renderUI({
     pca_y_choices <- setdiff(1:4, input$pca_x)
-    updateSelectInput(session, "pca_y", "y axis", choices = setNames(pca_y_choices, paste0("PC", pca_y_choices)))
-  })
+    selectInput("pca_y", "y axis", choices = setNames(pca_y_choices, paste0("PC", pca_y_choices)))
+           })
+  
   
   
   output$pcaPlot <- renderPlot({
-    g <- ggbiplot(ir.pca(), choices = c(as.numeric(input$pca_x), as.numeric(input$pca_y)),obs.scale = 1, var.scale = 1, groups = ir.species(), ellipse = TRUE, circle = TRUE) 
+    g <- ggbiplot(ir.pca(), choices = c(as.numeric(input$pca_x), as.numeric(input$pca_y)),obs.scale = 1, var.scale = 1, groups = ir.species(), ellipse = TRUE) 
     g <- g + scale_color_discrete(name = '') 
     g <- g + theme(legend.direction = 'horizontal', legend.position = 'top')
     print(g)
